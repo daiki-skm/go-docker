00b0c555139f7acb198f94889ac1a5f15fa73333
佐久間大空 <daikisakuma6040@gmail.com>
2021-04-11 19:37:45 +0900

    update: first

---
+++ first.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	// "time"
+	// "math/rand"
+	// "math"
+)
+
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func main() {
+	// fmt.Println("Hello, 世界")
+	// fmt.Println("The time is", time.Now())
+	// fmt.Println("My favorite number is", rand.Intn(110))
+	// fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	// fmt.Println(math.Pi)
+	// fmt.Println(add(42, 13))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+}
