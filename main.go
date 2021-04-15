7879c57d7392b1f1cc99ff32eb707aef71ca5a4b
佐久間大空 <daikisakuma6040@gmail.com>
2021-04-15 23:02:05 +0900

    update: add docker

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func swap(x, y string) (string, string) {
 }
 
 func main() {
-	// fmt.Println("Hello, 世界")
+	fmt.Println("Hello, 世界")
 	// fmt.Println("The time is", time.Now())
 	// fmt.Println("My favorite number is", rand.Intn(110))
 	// fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
