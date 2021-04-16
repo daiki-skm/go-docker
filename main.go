7a281a31c3c327c8c28ec109401dd28d0eadd4bb
佐久間大空 <daikisakuma6040@gmail.com>
2021-04-17 01:33:31 +0900

    fix: 4-17

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	// "math/cmplx"
 	// "time"
 	// "math/rand"
-	// "math"
+	"math"
 )
 
 // func add(x, y int) int {
@@ -36,20 +36,52 @@ import (
 
 // const Pi = 3.14
 
-const (
-	Big = 1 << 100
-	Small = Big >> 99
-)
+// const (
+// 	Big = 1 << 100
+// 	Small = Big >> 99
+// )
 
-func needInt(x int) int { return x*10 + 1 }
+// func needInt(x int) int { return x*10 + 1 }
 
-func needFloat(x float64) float64 { return x*0.1 }
+// func needFloat(x float64) float64 { return x*0.1 }
+
+// func sqrt(x float64) string {
+// 	if x < 0 {
+// 		return sqrt(-x) + "i"
+// 	}
+// 	return fmt.Sprint(math.Sqrt(x))
+// }
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	return lim
+}
 
 func main() {
-	fmt.Println(needInt(Small))
-	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Small))
-
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+	// fmt.Println(sqrt(2), sqrt(-4))
+	// sum := 1
+	// for sum < 100 {
+	// 	sum += sum
+	// }
+	// for ; sum < 100; {
+	// 	sum += sum
+	// }
+	// sum := 0
+	// for i := 0; i < 10; i++ {
+	// 	sum += 1
+	// }
+	// fmt.Println(sum)
+	// fmt.Println(needInt(Small))
+	// fmt.Println(needFloat(Small))
+	// fmt.Println(needFloat(Small))
 	// const World = "world"
 	// fmt.Println("Hello", World)
 	// fmt.Println("Happy", Pi, "Day")
