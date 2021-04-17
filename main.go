e6378418ecfffc970e4e8936c1616086cad8bacc
佐久間大空 <daikisakuma6040@gmail.com>
2021-04-17 12:57:42 +0900

    update: 4-17

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ package main
 
 import (
 	"fmt"
+	// "runtime"
 	// "math/cmplx"
-	// "time"
+	"time"
 	// "math/rand"
-	"math"
+	// "math"
 )
 
 // func add(x, y int) int {
@@ -52,20 +53,41 @@ import (
 // 	return fmt.Sprint(math.Sqrt(x))
 // }
 
-func pow(x, n, lim float64) float64 {
-	if v := math.Pow(x, n); v < lim {
-		return v
-	} else {
-		fmt.Printf("%g >= %g\n", v, lim)
-	}
-	return lim
-}
+// func pow(x, n, lim float64) float64 {
+// 	if v := math.Pow(x, n); v < lim {
+// 		return v
+// 	} else {
+// 		fmt.Printf("%g >= %g\n", v, lim)
+// 	}
+// 	return lim
+// }
 
 func main() {
-	fmt.Println(
-		pow(3, 2, 10),
-		pow(3, 3, 20),
-	)
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+	// fmt.Print("Go runs on ")
+	// switch os := runtime.GOOS; os {
+	// case "darwin":
+	// 	fmt.Println("OS X.")
+	// case "linux":
+	// 	fmt.Println("Linux.")
+	// default:
+	// 	fmt.Printf("%s.\n", os)
+	// }
+	// fmt.Println(
+	// 	pow(3, 2, 10),
+	// 	pow(3, 3, 20),
+	// )
 	// fmt.Println(sqrt(2), sqrt(-4))
 	// sum := 1
 	// for sum < 100 {
